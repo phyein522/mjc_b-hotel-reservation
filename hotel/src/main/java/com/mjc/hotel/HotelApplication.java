a3e677ae43d4a0fd0bc7000b4961de2b6eb9b35a
박혜인 <phyein522@gmail.com>
2026-06-23 21:38:31 +0900

    server test

--- hotel/src/main/java/com/mjc/hotel/HotelApplication.java
+++ hotel/src/main/java/com/mjc/hotel/HotelApplication.java
@@ -1,9 +1,11 @@
 package com.mjc.hotel;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.mjc.hotel.*.mapper")
 public class HotelApplication {
 
 	public static void main(String[] args) {
