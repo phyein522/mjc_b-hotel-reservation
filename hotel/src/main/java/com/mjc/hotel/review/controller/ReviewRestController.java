301ee58506aa284cca60375e8b6028f28f15cf28
smd030830 <smd030830@gmail.com>
2026-07-08 14:12:23 +0900

    refactor : 컨트롤러 api경로 수정

--- hotel/src/main/java/com/mjc/hotel/review/controller/ReviewRestController.java
+++ hotel/src/main/java/com/mjc/hotel/review/controller/ReviewRestController.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@RequestMapping("/api/v1/review")
+@RequestMapping("/api/review")
 public class ReviewRestController {
     @Autowired
     private ReviewService reviewService;
