7bd43fed7311016b7c3b23ed12ec13aca670945c
joominsang <joominsang54@gmail.com>
2026-07-09 14:05:38 +0900

    호텔 교통, 호텔 comment 추가

--- hotel/src/main/java/com/mjc/hotel/hotels/HotelEntity.java
+++ hotel/src/main/java/com/mjc/hotel/hotels/HotelEntity.java
@@ -16,47 +16,48 @@ import java.time.LocalTime;
 public class HotelEntity extends BaseEntity implements IHotel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "hotel_id", comment = "기본키")
     private Long hotelId;
 
-    @Column(nullable = false, length = 200)
+    @Column(nullable = false, length = 200, comment = "호텔 이름")
     private String name;
 
-    @Column(columnDefinition = "TEXT")
+    @Column(columnDefinition = "TEXT", comment = "설명")
     private String description;
 
-    @Column(nullable = false, length = 500)
+    @Column(nullable = false, length = 500, comment = "주소")
     private String address;
 
-    @Column(nullable = false, length = 100)
+    @Column(nullable = false, length = 100, comment = "도시")
     private String city;
 
-    @Column(nullable = false, length = 30)
+    @Column(nullable = false, length = 30, comment = "우편번호")
     private String zipCode;
 
-    @Column(nullable = false, length = 30)
+    @Column(nullable = false, length = 30, comment = "전화번호")
     private String phone;
 
-    @Column(nullable = false, length = 255)
+    @Column(nullable = false, length = 255, comment = "이메일")
     private String email;
 
-    @Column(nullable = false)
+    @Column(nullable = false, comment = "체크인 시간")
     private LocalTime checkIn;
 
-    @Column(nullable = false)
+    @Column(nullable = false, comment = "체크아웃 시간")
     private LocalTime checkOut;
 
-    @Column
+    @Column(comment = "별 등급")
     private Short starRate;
 
-    @Column(nullable = false)
+    @Column(nullable = false, comment = "운영 여부")
     private Boolean isActive;
 
-    @Column(nullable = false)
+    @Column(nullable = false, comment = "위도")
     private String latitude;
 
-    @Column(nullable = false)
+    @Column(nullable = false, comment = "경도")
     private String longitude;
 
-    @Column(nullable = false)
+    @Column(nullable = false, comment = "호텔 유형")
     private HotelTypeEnum type;
 }
