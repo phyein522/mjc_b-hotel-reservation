09bce1c6ddf0b958b958775de68acc6db32cafe4
dlwogus825 <dlwogus825@gmail.com>
2026-07-13 10:03:18 +0900

    rate controller test 추가
    rate controller test 추가

--- hotel/src/test/java/com/mjc/hotel/rates/controller/TestRatePolicyController.java
+++ hotel/src/test/java/com/mjc/hotel/rates/controller/TestRatePolicyController.java
@@ -6,6 +6,7 @@ import com.mjc.hotel.rates.service.RatePolicyService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
