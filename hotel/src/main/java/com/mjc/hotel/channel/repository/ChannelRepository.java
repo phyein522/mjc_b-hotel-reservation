c8dbcb420f1d8a4ccb5b941670b09ae987762793
박혜인 <phyein522@gmail.com>
2026-07-13 13:38:21 +0900

    chanel 삭제

--- hotel/src/main/java/com/mjc/hotel/channel/repository/ChannelRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mjc.hotel.channel.repository;
-
-import com.mjc.hotel.channel.entity.Channel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ChannelRepository extends JpaRepository<Channel, Long> {
-}
