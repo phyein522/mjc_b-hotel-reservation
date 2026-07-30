08cfc266cff41564e0dfcd4bf2c7d1e03807cddb
SoftAgape, Sincerus <sincerus@hanmail.net>
2026-07-30 12:31:01 +0900

    Toss 결제 처리 완료, 환불 기능은 없음

--- hotel/src/main/java/com/mjc/hotel/payment/dto/TossPaymentFailRequestDto.java
+++ hotel/src/main/java/com/mjc/hotel/payment/dto/TossPaymentFailRequestDto.java
@@ -1,10 +1,12 @@
 package com.mjc.hotel.payment.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class TossPaymentFailRequestDto {
-    private String orderId;
+    private String paymentKey;
     private String code;
     private String message;
 }
