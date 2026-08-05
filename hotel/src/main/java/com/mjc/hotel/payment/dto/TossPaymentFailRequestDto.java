ab79ac4ffd2b9c0e9404e897b7879ef0d5b084fe
dlwogus825 <dlwogus825@gmail.com>
2026-08-05 14:45:11 +0900

    취소
    취소

--- hotel/src/main/java/com/mjc/hotel/payment/dto/TossPaymentFailRequestDto.java
+++ hotel/src/main/java/com/mjc/hotel/payment/dto/TossPaymentFailRequestDto.java
@@ -2,10 +2,8 @@ package com.mjc.hotel.payment.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
 public class TossPaymentFailRequestDto {
     private String paymentKey;
